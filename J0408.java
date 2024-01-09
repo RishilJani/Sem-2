136c37a8eef86281ae0d6e6e54a3f9cf9842c08f
RishilJani <140903495+RishilJani@users.noreply.github.com>
2024-01-09 10:20:36 +0530

    Add files via upload

---
+++ J0408.java
@@ -0,0 +1,20 @@
+public class J0408 {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 6; i++) {
+            if(i<3){    
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("*");
+                }
+            }
+            else{
+                
+                for (int j = (5-i)-1; j >= 0; j--) {
+                    System.out.print("*");       
+                }
+            }
+            System.out.println("");
+            
+        }
+    }
+}

05af7c9a45a398044dffe04fb23eb6c66a61fec1
RishilJani <140903495+RishilJani@users.noreply.github.com>
2024-01-09 11:21:27 +0530

    Update J0302Palindrome.java

--- J0302Palindrome.java
+++ J0302Palindrome.java
@@ -1,40 +1,21 @@
 import java.util.Scanner;
+
 public class J0302Palindrome {
     public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("1. String \n2. number ");
-            
-            int n = sc.nextInt();
-            if (n==2) {
-                System.out.print("Enter a num: ");
-                int num = sc.nextInt();    
-                int a,i,r=0;
-                a = num;
-                while (num>0) {
-                    i = num%10;
-                    r = r * 10 + i;
-                    num = num / 10;
-                }
-                if(a == r){
-                    System.out.println("Number is Palindrome");
-                }
-                else{
-                    System.out.println("Number is not Palindrome");
-                }    
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a String:");
+        String str = sc.next();
+
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                System.out.println("not palindrome");
+                return;
             }
-            else{
-                System.out.print("Enter a String:");
-                String str = sc.next();
-            
-                for (int i = 0; i < str.length()/2; i++) {
-                   if(str.charAt(i) != str.charAt(str.length()-1-i)) {
-                        System.out.println("not palindrome");
-                        return;
-                    }
-                }
-                System.out.println("palindrome");
-                
-            }
+        }
+
+        System.out.println("palindrome");
+
         sc.close();
     }
 }

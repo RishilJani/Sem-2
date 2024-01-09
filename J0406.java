7e0331da01b787615434df8257075b1a24af4b69
RishilJani <140903495+RishilJani@users.noreply.github.com>
2024-01-09 10:12:25 +0530

    Add files via upload

---
+++ J0406.java
@@ -0,0 +1,12 @@
+public class J0406 {
+    public static void main(String[] args) {
+        String str = args[0];
+        int s = str.charAt(0); 
+        if(s>=65 && s<=91){
+            System.out.println(str);
+        }
+        else{
+            System.out.println("Error Occuerd");
+        }
+    }
+}

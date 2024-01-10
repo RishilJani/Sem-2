de40dd712d0f28e69e1997b9242d148dca1ad2b4
RishilJani <140903495+RishilJani@users.noreply.github.com>
2024-01-10 16:51:27 +0530

    Update and rename AreaOfCircle.java to J0204AreaOfCircle.java

--- J0204AreaOfCircle.java
+++ J0204AreaOfCircle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AreaOfCircle {
+public class J0204AreaOfCircle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter radius of circle: ");
